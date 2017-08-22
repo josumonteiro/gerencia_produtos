a03b04ed780314e2c4139b0098d4871c553a8f87
josue.minacio <josue.minacio@CAS511744W0725.senacsp.edu.br>
2017-08-22 20:12:58 -0300

    Projeto java inicial

---
+++ GerenciaProds/src/main/java/br/senac/utils/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.utils;
+
+/**
+ *
+ * @author Josu
+ */
+//Contém definições e parâmetros do sistema
+public class Constants {
+
+    //URL de conexão com o banco de dados
+    public static final String DB_ADDRESS
+            = "jdbc:mysql://35.185.61.97:3306/GerenciaProds";
+    //Usuário de conexão com o banco de dados
+    public static final String DB_USER = "aula1";
+    //Senha de conexão com o banco de dados
+    public static final String DB_PASS = "1234567890";
+}
