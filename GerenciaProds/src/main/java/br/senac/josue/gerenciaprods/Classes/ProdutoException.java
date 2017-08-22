f57a1c920e324c4a22cfa0c9e012194bff07f8bc
rafael.rsilva22 <rafael.rsilva22@CAS511780W0726.senacsp.edu.br>
2017-08-22 20:28:23 -0300

    Objeto e validações.

---
+++ GerenciaProds/src/main/java/br/senac/josue/gerenciaprods/Classes/ProdutoException.java
@@ -0,0 +1,19 @@
+package br.senac.josue.gerenciaprods.Classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author rafael.rsilva22
+ */
+public class ProdutoException extends Exception {
+    
+    public ProdutoException (String message){
+        super (message);
+    }
+}
