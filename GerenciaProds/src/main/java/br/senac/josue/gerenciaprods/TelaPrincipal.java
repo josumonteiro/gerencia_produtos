c0f4b475419c8dcd380ead8694a98fdb2e423923
Nakamura-PC <Nakamura-PC@DESKTOP-O3CVL0Q.senacsp.edu.br>
2017-08-22 21:46:39 -0300

    Padronização telas.

--- GerenciaProds/src/main/java/br/senac/josue/gerenciaprods/TelaPrincipal.java
+++ GerenciaProds/src/main/java/br/senac/josue/gerenciaprods/TelaPrincipal.java
@@ -5,8 +5,6 @@
  */
 package br.senac.josue.gerenciaprods;
 
-import javax.swing.JFrame;
-
 /**
  *
  * @author rafael.rsilva22
@@ -14,6 +12,8 @@ import javax.swing.JFrame;
 public class TelaPrincipal extends javax.swing.JFrame {
 
     Cadastrar cadastrarProduto = null;
+    Consultar consultarProd = null;
+
     /**
      * Creates new form TelaPrincipal
      */
@@ -45,8 +45,18 @@ public class TelaPrincipal extends javax.swing.JFrame {
         });
 
         jButton2.setText("Consulta");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Sair");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -102,6 +112,17 @@ public class TelaPrincipal extends javax.swing.JFrame {
         }
         cadastrarProduto.toFront();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        if (consultarProd == null || !consultarProd.isDisplayable()) {
+            consultarProd = new Consultar();
+        }
+        consultarProd.toFront();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
